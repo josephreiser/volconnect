942b75d6cc5aa5aef1d9cd2af209472ff572dd79
Joseph Reiser <josephreiser@Josephs-MacBook-Pro-7.local>
2022-11-23 13:04:22 -0500

    added some server side updates

--- mern/server/server.js
+++ mern/server/server.js
@@ -10,9 +10,9 @@ app.use(require("./routes/requests"));
 
 // get driver connection
 const dbo = require("./db/conn");
-app.listen(port, () => {
+app.listen(port, async () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
+  await dbo.connectToServer(function (err) {
     if (err) console.error(err);
   });
   console.log(`Server is running on port: ${port}`);
